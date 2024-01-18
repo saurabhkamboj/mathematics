25bd241c051ab2174fde1c3e682f466b05891db2
Saurabh Kamboj <skamboj97@gmail.com>
2024-01-18 21:37:52 +0530

    Modify file

--- kaprekar_routine.rb
+++ kaprekar_routine.rb
@@ -2,29 +2,35 @@
 
 KAPREKAR_CONSTANT = 6174
 
+def valid?(number)
+  number.match?(/[^0-9]/) ||
+    number.length != 4 || number.squeeze.length < 2
+end
+
 number = nil
 loop do
+  print 'Enter any four digit number: '
   loop do
-    print 'Enter any four digit number: '
     number = gets.chomp
 
-    break unless number.match?(/[^0-9]/) || number.length != 4
-    print "Error! Enter a valid number.\n\n"
+    break unless valid?(number)
+
+    print 'Error, enter a valid number! '
   end
 
   print "Number => #{number}\n\n"
 
   loop do
-    small_number = number.chars.sort.join
+    small_number = number.ljust(4, '0').chars.sort.join
     large_number = small_number.reverse
 
     number = (large_number.to_i(10) - small_number.to_i(10)).to_s
-    puts "#{large_number} - #{small_number} = #{number}"
 
+    puts "#{large_number} - #{small_number} = #{number}"
     break if number.to_i == KAPREKAR_CONSTANT
   end
 
-  print "Do you want to test another number? "
+  print 'Do you want to test another number? '
   break unless gets.chomp.include?('y')
 end
 
@@ -40,18 +46,14 @@ loop do
 
   digits.sort!
   small_number = 0
-  for digit in digits
-    small_number = small_number * 10 + digit
-  end
+  digits.each { small_number = small_number * 10 + _1 }
 
   digits.sort! { _2 <=> _1 }
   large_number = 0
-  for digit in digits
-    large_number = large_number * 10 + digit
-  end
+  digits.each { large_number = large_number * 10 + _1 }
 
   number = large_number - small_number
-  printf("%4d - %4d = #{number}\n", large_number, small_number)
 
+  printf("%4d - %4d = #{number}\n", large_number, small_number)
   break if number == KAPREKAR_CONSTANT
 end
